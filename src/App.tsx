1a97b30f763d94349f2063f11508648864146dfe
Сорок два <sorokdva.developer@gmail.com>
2025-01-25 18:05:59 +0100

    ✨ feat: add animate.css dependency to App.tsx
    Include animate.css for enhanced styling and animations.
    Updated `package-lock.json` to reflect the new dependency addition.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import 'react-toastify/dist/ReactToastify.css'
 import 'assets/vendor/nucleo/css/nucleo.css'
 import 'assets/vendor/font-awesome/css/fa.all.css'
+import 'animate.css'
 import 'styles/index.scss'
 import 'index.css'
 import 'styles/Toastify.css'
