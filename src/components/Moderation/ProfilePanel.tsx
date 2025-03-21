796771cfe3c1fc7b92d357ce56b223ff345558fe
Сорок два <sorokdva.developer@gmail.com>
2025-03-21 17:29:26 +0100

    🚀 feat: add moderation panel to Profile with permission checks
    Introduce a moderation panel to the Profile Details component. The panel dynamically toggles based on permissions (e.g., warn, bans), enhancing moderation tools for authorized users. Included styles and button interactions for a seamless user experience.

---
+++ src/components/Moderation/ProfilePanel.tsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { usePermissions } from 'hooks/usePermissions'
+
+const ProfilePanel: React.FC = () => {
+  const { checkPermission } = usePermissions()
+
+  const permissions = {
+    warn: checkPermission('site', 'warn'),
+    ban: checkPermission('site', 'ban'),
+    stalk: checkPermission('site', 'stalk'),
+    rename: checkPermission('site', 'rename'),
+    addPoints: checkPermission('site', 'addPoints'),
+    ip: checkPermission('site', 'ip'),
+    antecedents: checkPermission('site', 'antecedents'),
+    moderationNotes: checkPermission('site', 'moderationNotes'),
+    userRole: checkPermission('site', 'userRole'),
+    dcInfos: checkPermission('site', 'userRole'),
+    removeSignature: checkPermission('site', 'userRole'),
+  }
+
+  return (
+    <div className="moderation-panel">
+      { permissions.antecedents && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Antécédents
+          </a>
+        </div>
+      )}
+      { permissions.moderationNotes && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Notes sur le joueur
+          </a>
+        </div>
+      )}
+      { permissions.warn && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Avertir
+          </a>
+        </div>
+      )}
+      { permissions.stalk && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Surveiller
+          </a>
+        </div>
+      )}
+      { permissions.dcInfos && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Doubles-Comptes
+          </a>
+        </div>
+      )}
+      { permissions.rename && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Renommer
+          </a>
+        </div>
+      )}
+      { permissions.removeSignature && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Supprimer la signature
+          </a>
+        </div>
+      )}
+      { permissions.addPoints && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Ajouter des points
+          </a>
+        </div>
+      )}
+
+      { permissions.ip && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Voir l'IP
+          </a>
+        </div>
+      )}
+      { permissions.ban && (
+        <div className="moderation-buttons buttons">
+          <a className="button_secondary bgred" target="#">
+            Bannir
+          </a>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProfilePanel
