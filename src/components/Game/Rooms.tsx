6b0b5482ba1e9a3abb87c7f24875407fbed5fc2b
Сорок два <sorokdva.developer@gmail.com>
2025-05-02 14:41:40 +0200

    ✨ feat: disable button unless user is an admin
    Add a condition to disable the button if the user lacks admin rights.
    This improves UX by ensuring only authorized users can interact.

--- src/components/Game/Rooms.tsx
+++ src/components/Game/Rooms.tsx
@@ -607,7 +607,9 @@ const RoomList = () => {
                   <button
                     className={ `px-4 py-2
                     ${formData.type !== 0 ? 'bg-black/40 text-gray-400 rounded-lg border border-gray-700 ': 'bg-gradient-to-r from-green-600 to-green-800  text-white rounded-lg shadow-lg'} cursor-not-allowed` }
-                    onClick={() => setFormData((prev) => ({ ...prev, type: 0 }))}>
+                    onClick={() => setFormData((prev) => ({ ...prev, type: 0 }))}
+                    disabled={!user?.isAdmin}
+                  >
                     NORMALE
                   </button>
                   <button
