4eaabcf9519f1a8c244f91e843363b92f83f34b0
Сорок два <sorokdva.developer@gmail.com>
2025-07-06 00:30:22 +0200

    ⚙️ feat: add /payment/confirm route

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,7 @@ import ResetPassword from 'pages/ResetPassword'
 import ServiceUnavailable from 'pages/ServiceUnavailable'
 import UserSettingsPage from 'pages/UserSettings'
 import ValidateUser from 'pages/ValidateUser'
+import PaymentSuccessPage from 'pages/PaymentSuccess'
 
 import { ToastContainer } from 'react-toastify'
 import Notifier from 'components/Notifier'
@@ -160,6 +161,7 @@ const AppContent: React.FC = () => {
         <Route path="/game/:id" element={<GamePage />} />
         <Route path="/stations" element={<Guilds />} />
         <Route path="/station/:id" element={<Guild />} />
+        <Route path="/payment/confirm" element={<PaymentSuccessPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       {(!isAdminRoute && !isGameRoute) && <Footer />}
